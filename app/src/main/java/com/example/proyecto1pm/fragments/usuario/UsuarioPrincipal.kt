462fe207b3fc85e8cebebfe98a9c375359eb3315
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-10-21 17:41:26 -0600

    Fragments creados ahora si
    Aun faltan algunos

--- app/src/main/java/com/example/proyecto1pm/fragments/usuario/UsuarioPrincipal.kt
+++
@@ -1,4 +0,0 @@
-package com.example.proyecto1pm.fragments.usuario
-
-class UsuarioPrincipal {
-}
--- app/src/main/java/com/example/proyecto1pm/fragments/workoutplan/WorkoutPlanDetail.kt
+++
@@ -1,4 +0,0 @@
-package com.example.proyecto1pm.fragments.workoutplan
-
-class WorkoutPlanDetail {
-}
