5edad060ca07c4f3f3457d5d7daf8ce1a8454c92
jsantis <san21153@uvg.edu.gt>
2022-10-22 00:45:17 -0600

    algunas configuraciones adicionales

--- app/src/main/java/com/example/proyecto1pm/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1pm/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.proyecto1pm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav : BottomNavigationView
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        navController = findNavController(R.id.fragmentContainer_mainActivity)
         setContentView(R.layout.activity_main)
     }
 }
