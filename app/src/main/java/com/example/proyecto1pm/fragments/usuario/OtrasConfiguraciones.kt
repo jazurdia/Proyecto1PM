280533e486c1ee90fda6dd91c4c1e68839363cdd
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-10-23 09:26:10 -0600

    Everything is awesomee
    Everything is cool when ur part of a team
    Everything is awesomeeee

--- app/src/main/java/com/example/proyecto1pm/fragments/usuario/OtrasConfiguraciones.kt
+++ app/src/main/java/com/example/proyecto1pm/fragments/usuario/OtrasConfiguraciones.kt
@@ -5,11 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.proyecto1pm.R
 
 
 class OtrasConfiguraciones : Fragment() {
 
+    private lateinit var spinner_enfoque: Spinner
+    private lateinit var spinner_masa: Spinner
+    private lateinit var spinner_porcentajeGrasa: Spinner
+    private lateinit var spinner_altura: Spinner
+    private lateinit var spinner_edad: Spinner
+    //private lateinit var spinner_metaInicial: Spinner no sé porque no funciona.
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+            spinner_enfoque = findViewById(R.id.otrasconfiguraciones_spinner_enfoquedelusuario)
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.enfoque_del_usuario_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner_enfoque.adapter = adapter
+            }
+
+            spinner_masa = findViewById(R.id.otrasconfiguraciones_spinner_masausuario)
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.masa_del_usuario_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner_masa.adapter = adapter
+            }
+            spinner_porcentajeGrasa = findViewById(R.id.otrasconfiguraciones_spinner_porcentajedegrasa)
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.porcentaje_de_grasa_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner_porcentajeGrasa.adapter = adapter
+            }
+            spinner_altura = findViewById(R.id.otrasconfiguraciones_spinner_alturaUsuario)
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.altura_del_usuario_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner_altura.adapter = adapter
+            }
+            spinner_edad = findViewById(R.id.otrasconfiguraciones_spinner_edadUsuario)
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.edad_del_usuario_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner_edad.adapter = adapter
+            }
+
+
+        }
+
+        requireActivity().findViewById<View>(R.id.bottom_navigation).visibility = View.GONE
+    }
 
 
 
--- app/src/main/java/com/example/proyecto1pm/fragments/usuario/UsuarioMain.kt
+++ app/src/main/java/com/example/proyecto1pm/fragments/usuario/UsuarioMain.kt
@@ -5,56 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import com.example.proyecto1pm.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UsuarioMain.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UsuarioMain : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class UsuarioMain : Fragment (R.layout.fragment_usuario_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var username: TextView
+    private lateinit var racha: TextView
+    private lateinit var masaPerdida: TextView
+
+    private lateinit var btn_objetivosUsuarios: ImageButton
+    private lateinit var btn_actualizarDatos: ImageButton
+    private lateinit var btn_otrasConfiguraciones: ImageButton
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+
+            // ImageButtons que llevarán a los fragments de objetivos, actualizar datos y otras configuraciones
+            btn_objetivosUsuarios = findViewById(R.id.usuariomain_iconoObjetivoUsuario)
+            btn_actualizarDatos = findViewById(R.id.usuariomain_iconoActualizarDatos)
+            btn_otrasConfiguraciones = findViewById(R.id.usuariomain_iconoConfiguraciones)
+
+
+            // Recordar que son TextView. Estos son los TextView "editables" a la derecha.
+            username = findViewById(R.id.usuariomain_fotoUsuario)
+            racha = findViewById(R.id.usuariomain_dias)
+            masaPerdida = findViewById(R.id.usuariomain_kgperdidos)
+
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario_main, container, false)
+    private fun onClickListener() {
+        btn_objetivosUsuarios.setOnClickListener {
+            // Ir a fragment de objetivos
+        }
+
+        btn_actualizarDatos.setOnClickListener {
+            // Ir a fragment de actualizar datos
+        }
+
+        btn_otrasConfiguraciones.setOnClickListener {
+            // Ir a fragment de otras configuraciones
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UsuarioMain.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UsuarioMain().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
+
 }
