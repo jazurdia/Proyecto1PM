4eff1eeb87565a159e14b308abcac663d2a6e5a5
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-10-23 09:33:35 -0600

    Una arañita se columpiaba
    sobre la tela de una araña

--- app/src/main/java/com/example/proyecto1pm/fragments/usuario/UsuarioSecond.kt
+++ app/src/main/java/com/example/proyecto1pm/fragments/usuario/UsuarioSecond.kt
@@ -5,56 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.proyecto1pm.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [UsuarioSecond.newInstance] factory method to
- * create an instance of this fragment.
- */
-class UsuarioSecond : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class UsuarioSecond : Fragment(R.layout.fragment_usuario_second) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var pesoInicial: TextView
+    private lateinit var pesoActual: TextView
+    private lateinit var pesoDeseado: TextView
+    private lateinit var objetivoSemanal: TextView
+    private lateinit var nivelActividad: TextView
+    private lateinit var enfoqueActual: TextView
+
+    private lateinit var sesionesPorSemanas: TextView
+    private lateinit var minutosPorSesion: TextView
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+
+            pesoInicial = findViewById(R.id.usuariosecond_pesoinicial)
+            pesoActual = findViewById(R.id.usuariosecond_pesoactual)
+            pesoDeseado = findViewById(R.id.usuariosecond_pesodeseado)
+            objetivoSemanal = findViewById(R.id.usuariosecond_objetivoSemanal)
+            nivelActividad = findViewById(R.id.usuariosecond_nivelActividad)
+            enfoqueActual = findViewById(R.id.usuariosecond_enfoqueActual)
+            sesionesPorSemanas = findViewById(R.id.usuariosecond_sesionesPorSemana)
+            minutosPorSesion = findViewById(R.id.usuariosecond_MinutosPorSesion)
         }
+
+        actualizarDatos()
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario_second, container, false)
+    private fun actualizarDatos(){ // hay que definir métodos que vengan de la base de datos interna para actualizar los datos
+        pesoInicial.text = "80kg"
+        pesoActual.text = "70kg"
+        pesoDeseado.text = "60kg"
+        objetivoSemanal.text = "1kg"
+        nivelActividad.text = "Moderado"
+        enfoqueActual.text = "Perder peso"
+        sesionesPorSemanas.text = "3"
+        minutosPorSesion.text = "30"
     }
+    
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UsuarioSecond.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UsuarioSecond().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
