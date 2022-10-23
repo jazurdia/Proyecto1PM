8adf0bbe19f89468505826d2d34dc7adf0c661dc
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-10-23 00:17:16 -0600

    login y registro
    Aun faltan cosas

--- app/src/main/java/com/example/proyecto1pm/fragments/Login.kt
+++ app/src/main/java/com/example/proyecto1pm/fragments/Login.kt
@@ -5,15 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.proyecto1pm.R
+import com.google.android.material.textfield.TextInputLayout
 
 
 class Login : Fragment(R.layout.fragment_login) {
 
+    private lateinit var inputEmail: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+    private lateinit var buttonLogin: Button
+
+    private var username: String = "@username"
+    private var password: String = "@password"
+
+
 
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
+
+            view.apply {
+                inputEmail = findViewById(R.id.login_email)
+                inputPassword = findViewById(R.id.login_password)
+                buttonLogin = findViewById(R.id.login_continuarbtn)
+            }
+
             requireActivity().findViewById<View>(R.id.bottom_navigation).visibility = View.GONE
         }
+
+
+    private fun setListeners() {
+        buttonLogin.setOnClickListener {
+            loginIfUser(
+                email = inputEmail.editText!!.text.toString(),
+                password2 = inputPassword.editText!!.text.toString()
+            )
+        }
+    }
+
+    private fun loginIfUser(email: String, password2: String) {
+        if (email == username && password == password) {
+            //navigateToListScreen()
+        } else {
+            Toast.makeText(requireContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToWorkoutScreen() {
+        // falta definir el navController
+    }
+
+
 
 }
--- app/src/main/java/com/example/proyecto1pm/fragments/Registro.kt
+++ app/src/main/java/com/example/proyecto1pm/fragments/Registro.kt
@@ -5,56 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.proyecto1pm.R
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Registro.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Registro : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Registro : Fragment(R.layout.fragment_registro) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var inputCorreo: TextInputLayout
+    private lateinit var inputContraseña: TextInputLayout
+    private lateinit var inputConfirmarContraseña: TextInputLayout
+    private lateinit var boton: Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
+            inputCorreo = findViewById(R.id.textInputLayout)
+            inputContraseña = findViewById(R.id.registro_contrasena)
+            inputConfirmarContraseña = findViewById(R.id.textInputLayout2)
+        }
+
+    }
+
+    private fun setListeners() {
+        boton.setOnClickListener {
+            RegistrarNuevoUsuario(
+                correo = inputCorreo.editText!!.text.toString(),
+                contraseña = inputContraseña.editText!!.text.toString(),
+                confirmarContraseña = inputConfirmarContraseña.editText!!.text.toString()
+            )
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro, container, false)
+    private fun RegistrarNuevoUsuario(correo: String, contraseña: String, confirmarContraseña: String) {
+
+        if(contraseña == confirmarContraseña) {
+            // pasarle parámetros a la base de datos.
+        } else {
+            Toast.makeText(requireContext(), "Contraseña no coincide", Toast.LENGTH_SHORT).show()
+
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Registro.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Registro().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun navigateToLogInScreen() {
+        // falta definir el navController
     }
+
+
 }
