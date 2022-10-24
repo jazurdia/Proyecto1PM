f256d45f5d5a57de7de50fbc302325ca3f839aa6
jsantis <san21153@uvg.edu.gt>
2022-10-23 21:39:16 -0600

    recycler setup in workoutlist

--- app/src/main/java/com/example/proyecto1pm/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1pm/ui/activities/MainActivity.kt
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.proyecto1pm.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNav : BottomNavigationView
-    private lateinit var navController: NavController
+    private lateinit var controlador: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navController = findNavController(R.id.fragmentContainer_mainActivity)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer_mainActivity) as NavHostFragment
+        controlador = navHostFragment.navController
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/proyecto1pm/ui/fragments/workoutplan/WorkoutList.kt
+++ app/src/main/java/com/example/proyecto1pm/ui/fragments/workoutplan/WorkoutList.kt
@@ -5,56 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyecto1pm.Data.database.Excersise
+import com.example.proyecto1pm.Data.database.ExcersiseData
 import com.example.proyecto1pm.R
+import com.example.proyecto1pm.databinding.FragmentWorkoutListBinding
+import com.example.proyecto1pm.ui.adapters.AdapterWorkout
+import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WorkoutList.newInstance] factory method to
- * create an instance of this fragment.
- */
-class WorkoutList : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class WorkoutList : Fragment(R.layout.fragment_workout_list), AdapterWorkout.RecyclerViewWorkoutClickHandler {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var recycler: RecyclerView
+    private lateinit var list: MutableList<Excersise>
+    private lateinit var adapter: AdapterWorkout
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            recycler = view.findViewById(R.id.recycler_workouts)
+            initRecycler()
         }
+
+    private fun initRecycler() {
+        list = ExcersiseData.getExcersises()
+        adapter = AdapterWorkout(list, this)
+
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.setHasFixedSize(true)
+        recycler.adapter = adapter
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_list, container, false)
+    override fun onWorkoutClicked(excersise: Excersise) {
+        val action = FragmentWorkoutList.actionFragmentWorkoutListToFragmentWorkoutDetail(excersise)
+        requireView().findNavController().navigate(action)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WorkoutList.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WorkoutList().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
