d400fe1ebe559414316f114003cee88a2110d57a
jsantis <san21153@uvg.edu.gt>
2022-11-08 10:06:53 -0600

    gradle funcionando

---
+++ app/src/main/java/com/example/proyecto1pm/Data/Remote/Dto/FoodDto.kt
@@ -0,0 +1,5 @@
+package com.example.proyecto1pm.Data.Remote.Dto
+
+data class FoodDto(
+    val id : String,
+)
---
+++ app/src/main/java/com/example/proyecto1pm/Data/Remote/Dto/ResponseFoodDto.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto1pm.Data.Remote.Dto
+
+data class ResponseFoodDto(
+    val foods : List<FoodDto>,
+
+)
