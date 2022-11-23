6952f65dbe9abdafe342337fc9d3d20f97aff679
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-11-23 13:10:35 -0600

    Update WorkoutRepository.kt

--- app/src/main/java/com/example/proyecto1pm/Data/Repository/Workout/WorkoutRepository.kt
+++ app/src/main/java/com/example/proyecto1pm/Data/Repository/Workout/WorkoutRepository.kt
@@ -5,7 +5,7 @@ import com.example.proyecto1pm.Data.Resource
 
 interface WorkoutRepository {
 
-    suspend fun getWorkouts(WorkoutsReq:String): Resource<List<WorkOutEnt>>
+    suspend fun getWorkouts(): Resource<List<WorkOutEnt>>
 
 }
 
