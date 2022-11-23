44898e1bf5940c3f19a7a13bb733af028551464b
jsantis <san21153@uvg.edu.gt>
2022-11-22 19:59:12 -0600

    implementación prematura viewModel

--- app/src/main/java/com/example/proyecto1pm/ui/fragments/alimentacion/List/AlimentacionList.kt
+++ app/src/main/java/com/example/proyecto1pm/ui/fragments/alimentacion/List/AlimentacionList.kt
@@ -1,44 +1,80 @@
 package com.example.proyecto1pm.ui.fragments.alimentacion.List
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyecto1pm.Data.database.Food
 import com.example.proyecto1pm.Data.database.FoodData
 import com.example.proyecto1pm.R
+import com.example.proyecto1pm.databinding.FragmentAlimentacionListBinding
 import com.example.proyecto1pm.ui.adapters.AdapterFood
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+class AlimentacionList : Fragment(){
+    private lateinit var binding : FragmentAlimentacionListBinding
+    private val viewModel : AlimentacionListViewModel by viewModels()
 
-class AlimentacionList : Fragment(R.layout.fragment_alimentacion_list), AdapterFood.RecyclerViewFoodClickHandler {
-
-    private lateinit var recycler: RecyclerView
-    private lateinit var list: MutableList<Food>
-    private lateinit var adapter: AdapterFood
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-            recycler = view.findViewById(R.id.recycler_alimentacion)
-            initRecycler()
-            requireActivity().findViewById<View>(R.id.bottom_navigation).visibility = View.VISIBLE
-        }
-
-    private fun initRecycler() {
-        list = FoodData.getFoods()
-        adapter = AdapterFood(list, this)
-
-        recycler.layoutManager = L inearLayoutManager(requireContext())
-        recycler.setHasFixedSize(true)
-        recycler.adapter = adapter
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAlimentacionListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onFoodClicked(food: Food) {
-        //navigate with nav controller
-        // PABLO ESTO TAMBIEN LO CAMBIE.
-        val action = AlimentacionListDirections.actionAlimentacionList3ToAlimentacionDetails(food)
-        requireView().findNavController().navigate(action)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setObservers()
+        viewModel.getFoods()
+    }
+
+    private fun setObservers() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.PublicUiState.collect { state ->
+                handleState()
+            }
+        }
+    }
+
+    private fun handleState(state: AlimentacionListUiState){
+        when(state){
+            is AlimentacionListUiState.Success -> {
+                binding.recyclerAlimentacion.visibility = View.VISIBLE
+                binding.progressBarAlimentacionList.visibility = View.GONE
+                binding.toolbarAlimentacionList.visibility = View.VISIBLE
+                binding.constraintLayoutCalories.visibility = View.VISIBLE
+            }
+            is AlimentacionListUiState.Error -> {
+                //Show error
+                Toast.makeText(requireContext(),
+                    state.message,
+                    Toast.LENGTH_LONG).show()
+
+            }
+            AlimentacionListUiState.Loading -> {4
+                binding.recyclerAlimentacion.visibility = View.GONE
+                binding.progressBarAlimentacionList.visibility = View.VISIBLE
+                binding.toolbarAlimentacionList.visibility = View.GONE
+                binding.constraintLayoutCalories.visibility = View.GONE
+            }
+            AlimentacionListUiState.Default -> {
+                binding.recyclerAlimentacion.visibility = View.GONE
+                binding.progressBarAlimentacionList.visibility = View.GONE
+                binding.toolbarAlimentacionList.visibility = View.GONE
+                binding.constraintLayoutCalories.visibility = View.GONE
+            }
+        }
     }
 
 
